b4d83b0142f61d2b04f7ac2e407f7bdf9791f111
TheRune22 <rune.e.b.lejboelle@gmail.com>
2024-06-01 23:09:25 +0200

    Cleaned up TODOs

--- src/BTA.hs
+++ src/BTA.hs
@@ -3,29 +3,45 @@ module BTA where
 
 import Language.Haskell.Exts
 import Control.Monad.Reader
-
-import Utils
+import Control.Monad.Writer.Lazy
+import Control.Monad.RWS.Lazy
+import Data.Set (Set, member)
 import Debug.Trace (trace)
 
+import Utils
 
--- TODO: add static/dynamic division as input/(reader) monad
+-- Done?
+-- TODO: add static/dynamic division as input/(reader) monad7
+
+-- Do now
+-- TODO: implement handling of function calls in analyzeExp
+-- TODO: function for specializing single function name given dynamic args, maybe returning name of specialized function?
+-- TODO: also keep original module in reader?
+-- Reader or state for keeping track of implemented functions, maybe mapping function name and dynamic args to actual name of implementation?
+-- Writer for emitting specialized functions
+-- How to determine binding time of function call?
+
+-- Do later
+-- TODO: should also keep track of which vars are actually functions?
+-- TODO: just use bool for binding time, i.e. isDynamic?
+-- TODO: decide on and clean up monad stack
+-- TODO: make clean wrapper without monads, maybe merge with analyzeFunc or similar?
 -- TODO: drop or modify SrcSpanInfo?
--- TODO: preserve existing decls, or import them from the original module?
--- TODO: can everything be converted one-to-one, or must we sometimes generate more than one thing?
 -- TODO: implement missing pattern matches
--- TODO: if handling multiple decls, must know division for each decl
+-- TODO: take map of function name to dynamic args, enabling handling multiple decls at once? could just map over functions to handle instead?
 -- TODO: use ExpBracket or TExpBracket?
+-- TODO: copy original function definition? just import or duplicate?
+-- TODO: must also know name of function being specialized, and the name of the specialized function for recursion, or can all functions be handled equally?
 
--- TODO: should be able to call original function both at compile time and runtime? just import or duplicate?
-
--- TODO: need to keep track of all polyvariant specializations and the corresponding implementations
---  name should be available before implementation is generated, could have writer with a field for names and a field for implementations
---  need to be able to read and write names, so should maybe be state monad?
---  function analyzers could emit code in monad instead and same could be done when handling expressions calling functions
+-- Notes
+-- create (un)specialized versions of functions, as needed
+-- Generated code emitted in writer monad
+-- create new specializations when new calls are found
 --  only specialize local function definitions initially
 
--- TODO: handle SrcSpanInfo
 
+
+-- TODO: just use bool instead?
 -- TODO: map each variable to its binding time, add special cases for functions, are these also seen as vars?
 data BindingTime = Static | Dynamic
 
@@ -33,10 +49,9 @@ data BindingTime = Static | Dynamic
 -- TODO: instantiate l or drop it? probably won't need SrcSpanInfo, but could be used for Division?
 -- TODO: use QName, Name, or String?, drop l?
 type Division l = [(QNameLookup l, BindingTime)]
+--type Division l = Set (QNameLookup l)
 
--- TODO: rename? remove l
-type BTAMonad l = Reader (Division l)
-
+type FunctionEnv l = ()
 
 newtype QNameLookup l = QNameLookup (QName l)
 
@@ -46,41 +61,63 @@ instance Eq (QNameLookup l) where
   QNameLookup n1 == QNameLookup n2 = n1 =~= n2
 
 
--- TODO: should probably avoid SrcSpanInfo here
+-- TODO: rename? remove l
+type BTAMonad l = Reader (Division l)
+-- TODO: function env in state instead?
+--type BTAMonad l = ReaderT (Division l, FunctionEnv l) (Writer [Decl l])
+--type BTAMonad l = RWS (Division l) [Decl l] (FunctionEnv l)
+
+-- TODO: should probably avoid SrcSpanInfo here, rename to isDynamic and just return bool?
 bindingTime :: QName SrcSpanInfo -> BTAMonad SrcSpanInfo BindingTime
 bindingTime qName = do
   division <- ask
+--  if member (QNameLookup qName) division then
+--    pure Dynamic
+--  else
+--    pure Static
   case lookup (QNameLookup qName) division of
     Just bt -> pure bt
 --    If not found, assume static, since probably a static import
     Nothing -> pure Static
 
 
--- TODO: add clean wrapper taking initial division as input and running the monad
-analyzeModule :: Module SrcSpanInfo -> BTAMonad SrcSpanInfo (Module SrcSpanInfo)
--- TODO: handle other parts of the module
-analyzeModule (Module info moduleHead pragmas imports [decl]) = Module info moduleHead pragmas imports <$> mapM analyzeDecl [decl]
-analyzeModule _ = notImplementedError
 
 
+
+--analyzeModule :: Module SrcSpanInfo -> BTAMonad SrcSpanInfo (Module SrcSpanInfo)
+---- TODO: handle other parts of the module
+--analyzeModule (Module info moduleHead pragmas imports [decl]) = Module info moduleHead pragmas imports <$> mapM analyzeDecl [decl]
+--analyzeModule _ = notImplementedError
+--
+---- TODO: don't specialize if all args are static or dynamic, just copy code
+--analyzeDecl :: Decl SrcSpanInfo -> BTAMonad SrcSpanInfo (Decl SrcSpanInfo)
+--analyzeDecl (FunBind info [match]) = FunBind info <$> mapM analyzeMatch [match]
+--analyzeDecl _ = notImplementedError
+--
+--analyzeMatch :: Match SrcSpanInfo -> BTAMonad SrcSpanInfo (Match SrcSpanInfo)
+---- TODO: how to handle pats, only do simple cases?
+--analyzeMatch (Match info name pats rhs Nothing) = do
+----  TODO: could look up division for name here
+--  analyzedRhs <- analyzeRhs rhs
+--  pure $ Match info name pats analyzedRhs Nothing
+---- TODO: if Let is implemented, also handle binds here in same way?
+--analyzeMatch _ = notImplementedError
+--
+--analyzeRhs :: Rhs SrcSpanInfo -> BTAMonad SrcSpanInfo (Rhs SrcSpanInfo)
+--analyzeRhs (UnGuardedRhs info exp) = UnGuardedRhs info <$> analyzeExp exp
+--analyzeRhs _ = notImplementedError
+
+-- TODO: this should maybe use or replace above
+--analyzeFunc funcName dynamicArgs =
+-- where to get existing module from, and how to find right function?
+-- add signature to set of specialized functions, but first check if already there?
+-- initialize environment with dynamic args
+-- emit implementation in writer monad
+-- return name of specialized function?
+-- TODO: take QName or string for function name?
+-- TODO: take QName, String or argument numbers for dynamic args?
 -- TODO: don't specialize if all args are static or dynamic, just copy code
-analyzeDecl :: Decl SrcSpanInfo -> BTAMonad SrcSpanInfo (Decl SrcSpanInfo)
-analyzeDecl (FunBind info [match]) = FunBind info <$> mapM analyzeMatch [match]
-analyzeDecl _ = notImplementedError
 
-
-analyzeMatch :: Match SrcSpanInfo -> BTAMonad SrcSpanInfo (Match SrcSpanInfo)
--- TODO: how to handle pats, only do simple cases?
-analyzeMatch (Match info name pats rhs Nothing) = do
-  analyzedRhs <- analyzeRhs rhs
-  pure $ Match info name pats analyzedRhs Nothing
--- TODO: if Let is implemented, also handle binds here in same way?
-analyzeMatch _ = notImplementedError
-
-
-analyzeRhs :: Rhs SrcSpanInfo -> BTAMonad SrcSpanInfo (Rhs SrcSpanInfo)
-analyzeRhs (UnGuardedRhs info exp) = UnGuardedRhs info <$> analyzeExp exp
-analyzeRhs _ = notImplementedError
 
 
 -- TODO: maybe reader should not be used before this point, should be input instead?
@@ -88,9 +125,6 @@ analyzeRhs _ = notImplementedError
 
 -- TODO: start here
 -- TODO: must know division
--- TODO: must also know name of function being specialized, and the name of the specialized function for recursion,
---      or can all functions be handled equally?
---      with only 1 decl, there is only recursion, but let could be used to define new functions
 
 -- TODO: should check if all subexpressions are static
 -- TODO: do a first pass that simply annotates all expressions with their binding time, then a second pass that generates template haskell?
